5281618599509912600efb0ec56ee0e8d9f3a5d2
Shobhit <121398721+orpic@users.noreply.github.com>
2023-09-06 20:56:33 +0530

    app init

---
+++ app/src/main/java/app/shobhit/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.shobhit.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
